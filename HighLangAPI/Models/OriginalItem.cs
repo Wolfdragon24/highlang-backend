2bfa5c54a37563a1b103c5f8afe66f37d288b1c4
Wolfdragon24 <dragonbomber24@gmail.com>
2024-08-25 12:43:16 +1000

    Append SQL class base and documentation

---
+++ HighLangAPI/Models/OriginalItem.cs
@@ -0,0 +1,14 @@
+﻿// Models/TranslationItem.cs
+
+using System.ComponentModel.DataAnnotations;
+
+namespace HighLangAPI.Models
+{
+    public class OriginalItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Text { get; set; } = string.Empty;
+    }
+}
---
+++ HighLangAPI/Models/TranslationItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HighLangAPI.Models
+{
+    public class TranslationItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Lang { get; set; } = string.Empty;
+        [Required]
+        public string HTMLText { get; set; } = string.Empty;
+        [Required]
+        [ForeignKey("OriginalId")]
+        public OriginalItem OriginalItem { get; set; }
+        [Required]
+        public string AudioPath { get; set; } = string.Empty;
+    }
+}
